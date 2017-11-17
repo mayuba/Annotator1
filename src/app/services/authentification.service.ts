230a9c9bb229d9a71d8df80b3174e7fbbb0d4e25
josaphat mayuba <mayubandele@live.fr>
2017-11-16 20:44:43 -0500

    Description du projet
    Ajout de commentaire sur la description du projet

--- src/app/services/authentification.service.ts
+++ src/app/services/authentification.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-
+//Le service qui fournit les contantes et les fonctions requises pour la gestion de connexion.
 @Injectable()
 export class AuthentificationService {
 
