1fd4019b3c88afbcef63e2f4bda88e3abe03ca9a
josaphat mayuba <mayubandele@live.fr>
2017-11-21 14:27:15 -0500

    Ajout le composant de gestion de role
    Gestion de role sert à chercher, ajouter ou enlever l'utilisateur dans un projet.

---
+++ src/app/components/gestion-role/gestion-role.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-role',
+  templateUrl: './gestion-role.component.html',
+  styleUrls: ['./gestion-role.component.css']
+})
+export class GestionRoleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
