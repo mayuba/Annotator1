bfdf71fcfd3b1c96fe24861f63d446aaf95f7819
josaphat mayuba <mayubandele@live.fr>
2017-11-29 13:54:00 -0500

    correction de nom
    correction de l'identification authentification

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,5 +1,5 @@
 export * from './users.service';
 export * from './project.service';
 export * from './texte.service';
-export * from './authentication.service';
+export * from './authentification.service';
 export * from './annotation.service';
