ebe04c60cdb2daff5ddaa6ead31d2150717ae26b
josaphat mayuba <mayubandele@live.fr>
2017-11-29 13:09:59 -0500

    exportation des services dans un fichier
    le fichier index sert à l'importation unique des differents services
    pour importer un service il faut juste ajouter
    exemple  pour la classe ProjetService du fichier project.service.ts:
    '' import {ProjetService} from './services/index';'

---
+++ src/app/services/index.ts
@@ -0,0 +1,5 @@
+export * from './users.service';
+export * from './project.service';
+export * from './texte.service';
+export * from './authentication.service';
+export * from './annotation.service';
