5cb98833463fa306d8f627ea31080e938f021356
Mel <belm104@uqo.ca>
2017-11-29 21:35:13 -0500

    Ajout de modules AngularMaterial et modifs a l'interface project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import { NgModule } from '@angular/core';
 import { Routing } from './app.routing';
+import {MatButtonModule,MatToolbarModule, MatListModule, MatGridListModule,
+  MatMenuModule, MatStepperModule, MatCardModule, MatTabsModule, MatIconModule,
+MatInputModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 
 import { AngularFireModule } from 'angularfire2';
 
-//New imports to update based on AngularFire2 version 4
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { RegisterComponent } from './components/register/register.component';
@@ -53,6 +55,9 @@ export const firebaseConfig = {
   imports: [
     BrowserModule,
     FormsModule,
+    MatButtonModule,MatToolbarModule, MatListModule, MatGridListModule,
+    MatMenuModule, MatStepperModule, MatCardModule, MatTabsModule, MatIconModule,
+    MatInputModule,
     HttpModule,
     Routing,
     AngularFireModule.initializeApp(firebaseConfig),
