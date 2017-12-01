d183e01fbc06ec0f97bd833122b9044e3d5e0de7
josaphat mayuba <mayubandele@live.fr>
2017-11-30 22:21:37 -0500

    retourner la liste de projet d'un utilisateur
    ajout de la fonction oneSelf va retourner la liste de projet dont un utilisateur est annotateur ou administrateur.

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
 import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from 'angularfire2/firestore';
 import{Observable} from 'rxjs/Observable';
-
+import{User} from '../models/user.model'
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database-deprecated';
 
 @Injectable()
 
 export class ProjetService {
-  Project:any={};
+  selfProject: FirebaseListObservable<any[]>;
+  //user:User;
 
-
-  constructor() {
-
+  constructor(private database: AngularFireDatabase) {
+    this.selfProject = database.list('Usagers/test01');
   }
-
+  //recuperer la liste de projets d'un utilisateur
+  oneSelf(user: string){
+   return this.selfProject;
+  }
   //Obtention de la liste de tous les projets
   getAll(){
 
