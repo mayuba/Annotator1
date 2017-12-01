bc9b3a711bc50a0e6bf18b6a8e7cba44cc25f0b5
josaphat mayuba <mayubandele@live.fr>
2017-11-30 22:12:54 -0500

    ajout de l'affichage de la liste de projet d'un utilisateur

--- src/app/components/mes-projet/mes-projet.component.ts
+++ src/app/components/mes-projet/mes-projet.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ProjetService} from 'app/services/index';
 
 @Component({
   selector: 'app-mes-projet',
@@ -6,17 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mes-projet.component.css']
 })
 export class MesProjetComponent implements OnInit {
-  
+
   myprojects: File[]=[];
   model: any = {};
+  currentUser:string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private projet: ProjetService) {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
   }
 
+  ngOnInit() {
+
+  }
+  //chargement de la liste de projets du courant utilisateur
+  load() {
+       this.projet.oneSelf(this.currentUser).subscribe(myprojects => { this.myprojects = myprojects; });
+    }
+  search(word:string){
+
+  }
   edit(){ }
   IsAdmin(){}
   select(id: string){}
-  
+
 }
