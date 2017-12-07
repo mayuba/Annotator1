52f6f0e6f6113e6dfc1a8b7d3c82ef0a449eb18d
josaphat mayuba <mayubandele@live.fr>
2017-12-07 07:15:43 -0500

    rendre l'exportation plus simple
    index permet d'exporter les composants et l'importation se fait avec ce fichier index.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,24 +14,26 @@ import { AngularFirestoreModule } from 'angularfire2/firestore';
 //New imports to update based on AngularFire2 version 4
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
-import { RegisterComponent } from './components/register/register.component';
-import { LoginComponent } from './components/login/login.component';
-import { AnnotationComponent } from './components/annotation/annotation.component';
-import { AdminComponent } from './components/admin/admin.component';
 import { FooterComponent } from './shared/component/footer/footer.component';
 import { AlertDirective } from './directives/alert.directive';
-import { HomeComponent } from './components/home/home.component';
-import { ProjectComponent } from './components/project/project.component';
-import { DialogOverviewExampleDialog } from './components/project/project.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { HeaderComponent } from './shared/component/header/header.component';
-import { GestionRoleComponent } from './components/gestion-role/gestion-role.component';
-import { MesProjetComponent } from './components/mes-projet/mes-projet.component';
-import { GestionCategoriesComponent } from './components/gestionCategories/gestionCategories.component';
-import { GestionCorpusComponent } from './components/gestionCorpus/gestionCorpus.component';
+
+import { RegisterComponent ,
+         LoginComponent,
+         AnnotationComponent,
+         AdminComponent ,
+         HomeComponent ,
+         ProjectComponent ,
+         DialogOverviewExampleDialog ,
+         PageNotFoundComponent ,
+         GestionRoleComponent,
+         MesProjetComponent ,
+         GestionCategoriesComponent ,
+         GestionCorpusComponent,
+         TexteComponent
+       } from './components/index';
 //importation des services
 import {ProjetService} from './services/index';
-import { TexteComponent } from './components/texte/texte.component'
 
 export const firebaseConfig = {
   apiKey: "AIzaSyBxyDRCUs3jzK5sfSYj6443htnUaqKBp4w",
