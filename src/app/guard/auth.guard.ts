0b2a36f3f03081e2464f709300b4b35955f99ec7
josaphat mayuba <mayubandele@live.fr>
2017-12-08 12:37:13 -0500

    ajout de l'autorisation de routage
    autoriser le routage seulement si l'utilisateur est connecter

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,12 +1,24 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot,Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  canActivate(
+  /*canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     return true;
+  }*/
+  constructor(private router: Router) { }
+  canActivate() {
+  if (localStorage.getItem('currentUser')) {
+    // logged in so return true
+    return true;
+  }
+  else {
+    // not logged in so redirect to home page
+    this.router.navigateByUrl('/index', { queryParams: {path:'' }});
+    return false;
+    }
   }
 }
