0eb4dde4de3e177c7e9e581dbf8fa5879c5f80f6
josaphat mayuba <mayubandele@live.fr>
2017-12-08 04:29:37 -0500

    changement de nom du module Projects pour SearchProjet

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -10,4 +10,4 @@ export * from './mes-projet/mes-projet.component';
 export * from './gestionCategories/gestionCategories.component';
 export * from './gestionCorpus/gestionCorpus.component';
 export * from './texte/texte.component'
-export * from './projects/projects.component'
+export * from './SearchProjet/SearchProjet.component'
