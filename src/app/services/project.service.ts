bc2e05945c55b8aa4e1d4d90407f183f4f7129af
josaphat mayuba <mayubandele@live.fr>
2017-12-08 12:36:34 -0500

    suppresion des observations sur la table des utilisateurs
    projet va faire des requete que vers la table projet

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -4,42 +4,30 @@ import {AngularFirestore,
         AngularFirestoreDocument
        } from 'angularfire2/firestore';
 import {Observable} from 'rxjs/Observable';
-import {User} from '../models/user.model';
 import {Projet} from '../models/projet.model';
 import { Subject } from 'rxjs/Subject';
 
 
 @Injectable()
 export class ProjetService {
-  selfProject:Observable<User[]>;
   All: Observable<Projet[]>;
-
   projet: Array<string>;
-  user: Observable<User[]>;
-
   projetList: Observable<Projet[]>;
 
   private ProjetCollection: AngularFirestoreCollection<Projet>;
-  private userCollection: AngularFirestoreCollection<User>;
 
   currentUser:string;
 
   constructor(private afs: AngularFirestore) {
     //recupere le token de l'utilisateur courant
     this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
-
     this.ProjetCollection = afs.collection<Projet>('Projet');
-    this.userCollection = afs.collection<User>('User');
-
-    this.selfProject= this.userCollection.valueChanges();
     this.All = this.ProjetCollection.valueChanges();
   }
 
 
   //recuperer la liste de projets d'un utilisateur
   oneSelf() {
-    let tabProjet: Observable<User[]>;
-
     this.projetList =  this.ProjetCollection.valueChanges();
     return this.projetList;
   }
