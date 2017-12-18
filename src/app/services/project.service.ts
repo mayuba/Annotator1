2a5959f9baa8059e31c47fc8739e10257a939b66
Mel <belm104@uqo.ca>
2017-12-18 13:38:37 -0500

    gestion des membres seulement visible en mode 'admin projet'

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -27,7 +27,6 @@ export class ProjetService {
     this.allCouleurs = [
       {nom:'rouge', couleur:'#F44336'},
       {nom:'bleu', couleur:'#2196F3'},
-      {nom:'vert', couleur:'#4CAF50'},
       {nom:'jaune',couleur:'#FFEB3B'},
       {nom:'vert', couleur:'#4CAF50'},
       {nom:'violet', couleur:'#673AB7'},
