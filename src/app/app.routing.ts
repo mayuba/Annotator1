9c3517f927063e1b83f92ec613a7b2aee63ade70
Mayuba Ndele <josaphat.mayubandele@cgi.com>
2018-09-20 14:44:52 -0400

    fixer le package !important

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,69 +1,88 @@
-//Inspiré du code d'app.routing.ts du projet UQO-Annotator
+// Inspiré du code d'app.routing.ts du projet UQO-Annotator
 //
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {RouterTestingModule} from "@angular/router/testing";
+import { RouterTestingModule } from '@angular/router/testing';
 import { AuthGuard } from './guard/auth.guard';
-import {AppPreload} from './app.preload';
-//import { AuthGuard } from './_guards/index';
-//import { ComposeMessageComponent } from './compose-message.component';
-//import { HomeWelcomeComponent } from './home-welcome/index';
-import { RegisterComponent ,
-         LoginComponent,
-         AnnotationComponent,
-         IndexComponent,
-         HomeComponent ,
-         ProjectComponent ,
-         SearchProjetComponent ,
-         DialogOverviewExampleDialog ,
-         PageNotFoundComponent ,
-         GestionRoleComponent,
-         MesProjetComponent ,
-         GestionCategoriesComponent ,
-         GestionCorpusComponent,
-         TexteComponent
-       } from './components/index';
+import { AppPreload } from './app.preload';
+// import { AuthGuard } from './_guards/index';
+// import { ComposeMessageComponent } from './compose-message.component';
+// import { HomeWelcomeComponent } from './home-welcome/index';
+import {
+  RegisterComponent,
+  LoginComponent,
+  AnnotationComponent,
+  IndexComponent,
+  HomeComponent,
+  ProjectComponent,
+  SearchProjetComponent,
+  DialogOverviewExampleDialog,
+  PageNotFoundComponent,
+  GestionRoleComponent,
+  MesProjetComponent,
+  GestionCategoriesComponent,
+  GestionCorpusComponent,
+  TexteComponent
+} from './components/index';
 
 const appRoutes: Routes = [
-  { path: 'home', component: HomeComponent, pathMatch: 'full', canActivate: [AuthGuard] },
-  { path: 'index', component: IndexComponent,pathMatch: 'full' },
+  {
+    path: 'home',
+    component: HomeComponent,
+    pathMatch: 'full',
+    canActivate: [AuthGuard]
+  },
+  { path: 'index', component: IndexComponent, pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'mesprojet', component: MesProjetComponent , canActivate: [AuthGuard]},
+  {
+    path: 'mesprojet',
+    component: MesProjetComponent,
+    canActivate: [AuthGuard]
+  },
   { path: 'annotation', component: AnnotationComponent },
   { path: 'projet', component: ProjectComponent },
   { path: 'search', component: SearchProjetComponent },
-  { path: 'corpus', component: GestionCorpusComponent , canActivate: [AuthGuard]},
-  { path: 'categories', component: GestionCategoriesComponent , canActivate: [AuthGuard]},
-  { path: 'roles', component: GestionRoleComponent , canActivate: [AuthGuard]},
-  //activation lors de connexion
-  //A ne pas effacer
-  { path: '', component: HomeComponent, pathMatch: 'full', canActivate: [AuthGuard] },
-  { path: '', component: IndexComponent,pathMatch: 'full' },
+  {
+    path: 'corpus',
+    component: GestionCorpusComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'categories',
+    component: GestionCategoriesComponent,
+    canActivate: [AuthGuard]
+  },
+  { path: 'roles', component: GestionRoleComponent, canActivate: [AuthGuard] },
+  // activation lors de connexion
+  // A ne pas effacer
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+    canActivate: [AuthGuard]
+  },
+  { path: '', component: IndexComponent, pathMatch: 'full' },
   //  { path: 'compose', component: ComposeMessageComponent, outlet: 'popup' },
-//  { path: 'logout', component: HomeWelcomeComponent },
-//  { path: '', component: HomeComponent, /*pathMatch: 'full', canActivate: [AuthGuard]*/ },
-//  { path: '', component: HomeWelcomeComponent, pathMatch: 'full' },
+  //  { path: 'logout', component: HomeWelcomeComponent },
+  //  { path: '', component: HomeComponent, /*pathMatch: 'full', canActivate: [AuthGuard]*/ },
+  //  { path: '', component: HomeWelcomeComponent, pathMatch: 'full' },
   // otherwise page not found
   { path: '**', component: PageNotFoundComponent }
 ];
 @NgModule({
   imports: [
-    [ RouterModule.forRoot(appRoutes,
-      {
+    [
+      RouterModule.forRoot(appRoutes, {
         enableTracing: false, // <-- debugging purposes only
-        preloadingStrategy: AppPreload,
-
-      }) ]
+        preloadingStrategy: AppPreload
+      })
+    ]
   ],
-  exports: [
-    RouterModule
-  ],
-  providers: [
-    AppPreload
-  ]
+  exports: [RouterModule],
+  providers: [AppPreload]
 })
-export class AppRouting { }
-//export const Routing = RouterModule.forRoot(appRoutes);
+export class AppRouting {}
+// export const Routing = RouterModule.forRoot(appRoutes);
 
 export const RoutingTest = RouterTestingModule.withRoutes(appRoutes);
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBxyDRCUs3jzK5sfSYj6443htnUaqKBp4w",
-    authDomain: "annotateur-9b363.firebaseapp.com",
-    databaseURL: "https://annotateur-9b363.firebaseio.com",
-    projectId: "annotateur-9b363",
-    storageBucket: "annotateur-9b363.appspot.com",
-    messagingSenderId: "581480444578"
+    apiKey: 'AIzaSyBxyDRCUs3jzK5sfSYj6443htnUaqKBp4w',
+    authDomain: 'annotateur-9b363.firebaseapp.com',
+    databaseURL: 'https://annotateur-9b363.firebaseio.com',
+    projectId: 'annotateur-9b363',
+    storageBucket: 'annotateur-9b363.appspot.com',
+    messagingSenderId: '581480444578'
   }
 };
